8612015d3ce1ec17e169e6201304cd58e893690c
yodadaenerys <yoda.daenerys@gmail.com>
2022-02-25 11:05:34 -0500

    initial commit from yoda

---
+++ RapidReacts/src/main/java/frc/robot/commands/RobotMap.java
@@ -0,0 +1,19 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+/** Add your docs here. */
+public class RobotMap{ 
+    //Joystick
+    public static final int JOYSTICK_CHANNEL = 0;
+    public static final int JOYSTICK_FORWARD_AXIS = 1;
+    public static final int JOYSTICK_TURN_AXIS = 4;
+
+    // Drivetrain
+    public static final int DRIVE_MOTOR_LEFT_FRONT = 1;
+    public static final int DRIVE_MOTOR_LEFT_REAR = 2;
+    public static final int DRIVE_MOTOR_RIGHT_FRONT = 3;
+    public static final int DRIVE_MOTOR_RIGHT_REAR= 4;
+}
---
+++ RapidReacts/src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.RobotMap;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+  /*
+  private final CANSparkMax LeftMotorFront = new CANSparkMax(RobotMap.DRIVE_MOTOR_LEFT_FRONT, MotorType.kBrushless); 
+  private final CANSparkMax LeftMotorRear = new CANSparkMax(RobotMap.DRIVE_MOTOR_LEFT_REAR, MotorType.kBrushless);
+  private final CANSparkMax RightMotorFront = new CANSparkMax(RobotMap.DRIVE_MOTOR_RIGHT_FRONT, MotorType.kBrushless);
+  private final CANSparkMax RightMotorRear = new CANSparkMax(RobotMap.DRIVE_MOTOR_RIGHT_REAR, MotorType.kBrushless);
+*/
+private final PWMSparkMax LeftMotorFront = new PWMSparkMax(0);
+private final PWMSparkMax RightMotorFront = new PWMSparkMax(1);
+
+
+
+  /** Creates a new DrivetrainSubsystem. */
+  public DrivetrainSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+
+
+public void arcadeDrive(double forwardPower, double turnPower) {
+  double driveLeftPower = forwardPower - turnPower; 
+  double driveRightPower = forwardPower + turnPower;
+
+  LeftMotorFront.set(driveLeftPower * -1);
+  //LeftMotorRear.set(driveLeftPower * -1);
+  RightMotorFront.set(driveRightPower);
+  //RightMotorRear.set(driveRightPower);
+
+}
+}
